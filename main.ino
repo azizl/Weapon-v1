5070889efd1f268b3850694d912e2c64a70d7338
Aziz Lokhandwala <36584897+azizl@users.noreply.github.com>
2019-05-24 03:37:28 +0530

    Update main.ino

--- main.ino
+++ main.ino
@@ -1,8 +1,17 @@
+//Project by Ashfaque Khan and Aziz Lokhandwala
+//Code by Aziz Lokhandwala
+//Here output are servo and led both but I am still figuring out to get exact location of target so that servo could point out to target direction
+
+#include <Servo.h>
+Servo myservo;
+
 char val;
 int ledPin = 13;
+
 void setup() {
   pinMode(ledPin, OUTPUT);
   Serial.begin(9600);
+  myservo.attach(9);
 }
 
 void loop() {
@@ -11,9 +20,13 @@ if(Serial.available()){
 }
 if(val=='1'){
   digitalWrite(ledPin,HIGH);
+  val = map(val, 0, 1023,0,180);
+  myservo.write(val);
 }
 else{
   digitalWrite(ledPin,LOW);
+   val = map(val, 0, 1023,180,0);
+  myservo.write(val);
 }
 delay(20);
 }
